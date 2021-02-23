2b18bfbfc7bf78f5bb2331407234330016e642b6
bayan-alkhatib <bayankhatibtr@gmail.com>
2021-02-23 15:36:18 +0200

    adding js file

---
+++ dynamic.js
@@ -0,0 +1,14 @@
+alert("Welcome to Business Plan Website");
+
+prompt("are you a start up");
+
+var emailAddress=prompt("Please insert your email");
+console.log(emailAddress);
+
+var  navBar=prompt("would you prefer further information about Business plan");
+
+if (navBar.toLowerCase()=="yes"){console.log("1")}
+else{console.log("0")};
+
+confirm ("we encourage you to visit the links in the navigation bar");
+
